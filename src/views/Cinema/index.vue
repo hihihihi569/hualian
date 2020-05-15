4f835adcf7be7b70795a79ae082a580e2f9f220d
Honey-buddy <2769375451@qq.com>
2020-05-15 16:41:10 +0800

    完成基本框架

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="main">
+    <Header />
+    <div id="content">
+      <div class="cinema_menu">
+        <router-link tag="div" to="/cinema/pcity" class="city_switch">
+          全城
+          <i class="iconfont icon-lower-triangle"></i>
+        </router-link>
+        <router-link tag="div" to="/cinema/pro" class="brand_swtich">
+          品牌
+          <i class="iconfont icon-lower-triangle"></i>
+        </router-link>
+        <router-link tag="div" to="/cinema/feu" class="feature_switch">
+          特色
+          <i class="iconfont icon-lower-triangle"></i>
+        </router-link>
+      </div>
+    <Citylist />
+    </div>
+    <TarBar />
+  </div>
+</template>
+<script>
+import Header from '@/components/Header'
+import TarBar from '@/components/TarBar'
+import Citylist from '@/components/Citylist'
+export default {
+  name: 'cinema',
+  components: {
+    Header,
+    TarBar,
+    Citylist
+  }
+}
+</script>
+<style>
+#content {
+  flex: 1;
+  overflow: auto;
+  margin-bottom: 50px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+#content .cinema_body{ flex:1; overflow:auto;}
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="main">
+  <Header title="我的花脸"/>
+  <div id="content">
+    <Login />
+  </div>
+  <TarBar/>
+</div>
+</template>
+<script>
+import Header from '@/components/Header'
+import TarBar from '@/components/TarBar'
+import Login from '@/components/Login'
+export default {
+  name: 'mine',
+  components: {
+    Header,
+    TarBar,
+    Login
+  }
+}
+</script>
+<style>
+
+</style>
